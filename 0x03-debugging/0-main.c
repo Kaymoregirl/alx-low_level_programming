37dea117350d9c3cd851ff94995dd8a16079ae28
Kamogelo segone <kamogelosegone3@gmail.com>
2023-07-19 06:12:23 -0700

    Debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,13 @@
 #include "main.h"
 
-/**
- * main - Test function for positive or negative
- *Return: 0
- */
-int Main(void)
+void positive_or_negative(int i)
 {
-	int i;
+	if (i > 0)
+		printf("%d is positive\n", i);
 
-	i = 0;
-	Positive_or_negative(i);
-	return (0);
+	else if (i < 0)
+		printf("%d is negative\n" i);
+
+	else
+		printf("%d is zero\n", i);
 }
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -6,6 +6,7 @@
 int main(void)
 {
 	int i;
+
 	printf("Infinite loop incoming :(\n");
 	i = 10;
 	/*while (i < 10)*/
