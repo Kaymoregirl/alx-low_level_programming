2040b161e767662c925f2999203f7e7221197578
Kamogelo segone <kamogelosegone3@gmail.com>
2023-07-24 11:47:57 -0700

    _putchar.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *_atoi - converts a string to an integer
+ * __atoi - converts a string to an integer
  *@s: string to be converted
  *
  *Return: the int converted from the string
@@ -10,6 +10,7 @@
 int _atoi(char *s)
 {
 	int i, d, n, len, f, digit;
+	int _atoi
 
 	i = 0;
 	d = 0;
@@ -33,14 +34,14 @@ int _atoi(char *s)
 				digit = -digit;
 			n = n * 10 + digit;
 			f = 1;
-			if (s[ i + 1] < '0' || s[i + 1] > '9')
+			if (s[i + 1] < '0' || s[i + 1] > '9')
 				break;
 			f = 0;
 		}
 		i++;
 	}
 
-	if (f ==0)
+	if (f == 0)
 		return (0);
 
 	return (n);
