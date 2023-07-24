6c0f79391b27d9015a8be85ebfe1accb81df9dc8
Kamogelo segone <kamogelosegone3@gmail.com>
2023-07-24 08:56:33 -0700

    Pointers arrays strings

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+nclude "main.h"
+
+/**
+ *_strlen - return the length of the string
+ *@s: string
+ *Return: length
+ */
+
+int_strlen(char *s)
+{
+	int longi = 0;
+
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+
+	return (longi);
+}
