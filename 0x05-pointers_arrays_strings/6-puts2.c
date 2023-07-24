a51cf4a1752ad74e411cf4256ee0ff95b232610a
Kamogelo segone <kamogelosegone3@gmail.com>
2023-07-24 12:13:59 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -5,12 +5,13 @@
  *starting with the first one
  *@str: input
  *Return: print
+ */
 
- void puts2(char *str)
- {
- 	int longi = 0;
+void puts2(char *str)
+{
+	int longi = 0;
 	int t = 0;
-	charn *y = str;
+	char *y = str;
 	int o;
 
 	while (*y != '\0')
