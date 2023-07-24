cfcd5d81d9552790945241979111f605ac7544ee
Kamogelo segone <kamogelosegone3@gmail.com>
2023-07-24 12:27:08 -0700

    8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -3,7 +3,7 @@
 /**
  *print_array - a function that prints n elements of an array
  *@a: array name
- *@n: is the number of elements of the array to be printed
+ *@n: is the number of elements 0f the array to be printed
  *Return: a and n inputs
  */
 
