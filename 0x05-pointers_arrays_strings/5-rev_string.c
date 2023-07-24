a6c86d03e872eb7542356e176a2fec8d8a1f9cac
Kamogelo segone <kamogelosegone3@gmail.com>
2023-07-24 10:29:21 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,13 +6,13 @@
  *Return: String in reverse
  */
 
-void rev_ string(char *s)
+void rev_string(char *s)
 {
 	char rev = s[0];
 	int counter = 0;
 	int i;
 
-	while ([counter] != '\0')
+	while (s[counter] != '\0')
 	counter++;
 	for (i = 0; i < counter; i++)
 	{
