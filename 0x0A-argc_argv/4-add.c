d2e633c5c5b951d7dbebf35530752c8106ee704e
Kamogelo segone <kamogelosegone3@gmail.com>
2023-08-04 14:48:15 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,15 +1,16 @@
-#include < stdio.h> 
-#include < stdlib.h> 
-#include < ctype.h>
-#include < string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
 int check_num(char *str)
 {
 	unsigned int count;
+
 	count = 0;
 	while (count < strlen(str))
 	{
-		if (!isdigit(str[count]))
+	if (!isdigit(str[count]))
 	{	
 	return (0);
 	}
@@ -27,7 +28,7 @@ int main(int argc, char *argv[])
 	count = 1;
 	while (count < argc)
 	{
-		if(check_num(argv[count]))
+		if (check_num(argv[count]))
 	{	
 		str_to_int = atoi(argv[count]);
 		sum += str_to_int;
