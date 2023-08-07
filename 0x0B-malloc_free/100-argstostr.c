3a737034cffe406e17b058befba6807cdf0e9cab
Kaymoregirl <135054066+Kaymoregirl@users.noreply.github.com>
2023-08-07 22:46:22 +0200

    Update 100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -3,34 +3,34 @@
 
 /**
  * argstostr - main entry
- * @ac: int input
- * @av: double pointer array
+ * @bc: int input
+ * @ad: double pointer array
  * Return: 0
  */
 
-char *argstostr(int ac, char **av)
+char *argstostr(int bc, char **ad)
 {
 	int i, n, r = 0, l = 0;
 	char *str;
 
-	if (ac == 0 || av == NULL)
+	if (bc == 0 || ad == NULL)
 		return (NULL);
 
-	for (i = 0; i < ac; i++)
+	for (i = 0; i < bc; i++)
 	{
-		for (n = 0; av[i][n]; n++)
+		for (n = 0; ad[i][n]; n++)
 			l++;
 	}
-	l += ac;
+	l += bc;
 
 	str = malloc(sizeof(char) * l + 1);
 	if (str == NULL)
 		return (NULL);
-	for (i = 0; i < ac; i++)
+	for (i = 0; i < bc; i++)
 	{
-	for (n = 0; av[i][n]; n++)
+	for (n = 0; ad[i][n]; n++)
 	{
-		str[r] = av[i][n];
+		str[r] = ad[i][n];
 		r++;
 	}
 	if (str[r] == '\0')
