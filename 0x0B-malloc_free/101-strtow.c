80881eb0427bd2ff1d31285f75f10836452d69ba
Kamogelo segone <kamogelosegone3@gmail.com>
2023-08-07 12:47:48 -0700

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,4 +1,4 @@
-#include <stdlib.h>W34I
+#include <stdlib.h>
 #include "main.h"
 
 /**
@@ -13,9 +13,17 @@ int count_word(char *s)
 	return (w);
 
 }
+/**
+ * **strtow - splits a string into word
+ * @str: string to split
+ *
+ * Return: pointer to an array of strings (Success)
+ * or NULL (Error)
+ */
 char **strtow(char *str)
 {
-	char **matrix, *tmp, int i, k = 0, len = 0, words, c = 0, start, end;
+	char **matrix, *tmp;
+	int i, k = 0, len = 0, words, c = 0, start, end;
 
 	while (*(str + len))
 		len++;
